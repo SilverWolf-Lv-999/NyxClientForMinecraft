ba8a6b10ab0a1ce707b174ca6ab0e53641fb7be7
DaiChang <diyouyou345@outlook.com>
2026-07-06 19:54:08 +0800

    添加NanoVG

--- src/main/java/io/github/seraphina/nyxclient/Client.java
+++ src/main/java/io/github/seraphina/nyxclient/Client.java
@@ -4,10 +4,7 @@ import io.github.seraphina.nyxclient.events.api.EventManager;
 import io.github.seraphina.nyxclient.events.api.EventTarget;
 import io.github.seraphina.nyxclient.events.impl.SetScreenEvent;
 import io.github.seraphina.nyxclient.manager.*;
-import io.github.seraphina.nyxclient.ui.mainui.MainUI;
 import io.github.seraphina.nyxclient.utility.render.Shaders;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.TitleScreen;
 
 public class Client {
     public static final Client INSTANCE = new Client();
@@ -20,22 +17,9 @@ public class Client {
         RotationManager.INSTANCE.getClass();
         Shaders.init();
         FontManager.init();
-        new Thread(() -> {
-            while (true) {
-                System.out.println(Minecraft.getInstance().screen);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 
     @EventTarget
     public static void setScreen(SetScreenEvent event) {
-        if (event.getScreen() instanceof TitleScreen) {
-            event.setScreen(new MainUI());
-        }
     }
 }
