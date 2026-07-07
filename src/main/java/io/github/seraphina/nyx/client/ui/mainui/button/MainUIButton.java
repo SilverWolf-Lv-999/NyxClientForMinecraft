143a0344772d4023d5a2311f0f391ec2bf34696c
DaiChang <diyouyou345@outlook.com>
2026-07-07 08:53:02 +0800

    新增

---
+++ src/main/java/io/github/seraphina/nyx/client/ui/mainui/button/MainUIButton.java
@@ -0,0 +1,29 @@
+package io.github.seraphina.nyx.client.ui.mainui.button;
+
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.input.InputWithModifiers;
+import net.minecraft.network.chat.Component;
+
+public final class MainUIButton extends AbstractButton {
+    public MainUIButton(int p_93365_, int p_93366_, int p_93367_, int p_93368_, Component p_93369_) {
+        super(p_93365_, p_93366_, p_93367_, p_93368_, p_93369_);
+    }
+
+    @Override
+    public void onPress(InputWithModifiers inputWithModifiers) {
+
+    }
+
+    @Override
+    protected void renderContents(GuiGraphics guiGraphics, int i, int i1, float v) {
+
+    }
+
+    @Override
+    protected void updateWidgetNarration(NarrationElementOutput narrationElementOutput) {
+
+    }
+}
--- src/main/java/io/github/seraphina/nyx/mod/NyxMod.java
+++ src/main/java/io/github/seraphina/nyx/mod/NyxMod.java
@@ -1,18 +1,23 @@
 package io.github.seraphina.nyx.mod;
 
 import com.mojang.logging.LogUtils;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(NyxMod.MODID)
+@OnlyIn(Dist.CLIENT)
 public class NyxMod {
     public static final String MODID = "nyxclient";
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public NyxMod(IEventBus modEventBus, ModContainer modContainer) {
-
+        LOGGER.info(MODID + "  initialized");
+        modContainer.getModInfo().getModId();
+        modEventBus.unregister(this);
     }
 }
